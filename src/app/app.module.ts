e0607f9fd16982e4d8885fbab7f6082e1b46a5c8
Luis Mayta <luis.mayta@gmail.com>
2020-11-21 03:11:06 -0500

    21/11/2020 enlace zoom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage';
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
     StatusBar,
     SplashScreen,
     InAppBrowser,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SocialSharing
   ],
   bootstrap: [AppComponent]
 })
--- src/app/pages/zoom/zoom.page.ts
+++ src/app/pages/zoom/zoom.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { Recursos } from 'src/app/inerfaces/interfaces';
 import { DrupalService } from 'src/app/services/drupal.service';
+import { ActionSheetController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+
 
 @Component({
   selector: 'app-zoom',
@@ -12,15 +15,45 @@ export class ZoomPage implements OnInit {
 
   recursos:Recursos;
 
-  constructor(private drupal:DrupalService, private iab:InAppBrowser) { }
+  constructor(private drupal:DrupalService, 
+    private iab:InAppBrowser,
+    private actionSheetCtrl: ActionSheetController,
+    private socialSharing: SocialSharing) { }
 
   ngOnInit() {
     this.drupal.getRecursos('zoom').subscribe(resp=>{
       this.recursos = resp;
     });
   }
-  mostrarRecurso(i:string){
+  abrirZoom(i:string){
     const browser = this.iab.create(this.recursos[i].enlace,'_system');
   }
-
+  copiarLink(i:string){
+    
+    this.socialSharing.share(
+      this.recursos[i].titulo,'asunto','',this.recursos[i].enlace
+    );
+  }
+  async Action(i:string) {
+      const actionSheet = await this.actionSheetCtrl.create({
+        header: 'Escoge una opción',
+        cssClass: 'my-custom-class',
+        buttons: [{
+          text: 'Abrir Zoom',
+          icon: 'videocam',
+          handler: () => {
+            this.abrirZoom(i);
+          }
+        }, {
+          text: 'Compratir',
+          icon: 'share-social',
+          handler: () => {
+            this.copiarLink(i)
+          }
+        }]
+      });
+      await actionSheet.present();
+  }
+  
+  
 }
